a44a16737c78b3622239edc5ca1d73b901948fa9
damanpreetkaur17 <damanpreetk5162@gmail.com>
2025-03-30 14:18:55 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,9 +1,9 @@
 // Your code here...
 int isPrime(int t){
     if(t==2)
-    return 1;
-    else if (t%2==0)
-    return 1;
-    else 
     return 0;
+    else if (t%2==0)
+    return 0;
+    else 
+    return 1;
 }
